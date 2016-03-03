daf6f039ef1c6ace1ebcbee62b91bf961b332a92
Gabrielle <gabivk@gmail.com>
2016-03-03 15:31:29 +0000

    ATTRIBUTE REVELATION

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,14 +2,25 @@ require 'docking_station'
 
 describe DockingStation do
 
-it { is_expected.to respond_to 'release_bike'}
+  it { is_expected.to respond_to 'release_bike'}
 
-it 'get bike' do
-  expect(subject.release_bike).to be_a Bike
-end
+  it 'get bike' do
+    expect(subject.release_bike).to be_a Bike
+  end
 
-it 'expects the bike to be working' do
-expect(Bike.new).to be_working
-end
+  it { expect(Bike.new).to be_working }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike)}
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'shows docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
