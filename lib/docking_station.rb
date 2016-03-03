023b7ccef63ec7d541d8b327e9a657f5abfbecc1
Gabrielle <gabivk@gmail.com>
2016-03-03 18:47:22 +0000

    GAAAHHHHHHH

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,22 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
 
   def initialize
     @bikes = []
   end
 
   def release_bike
-    raise "no bikes bitches" unless @bike
+    raise "no bikes bitches" if @bikes.size == 0
     @bike
+    @bikes.pop
   end
 
   def dock(bike)
-    raise "no space to dock bike" if @bike
-    @bike = bike
+    raise "no space to dock bike" if @bikes.size == 20
+    @bikes << bike
+
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,18 +10,19 @@ describe DockingStation do
 
   it { expect(Bike.new).to be_working }
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike)}
+  # it { is_expected.to respond_to(:bike)}
 
   it 'docks a bike' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    subject.dock(bike)
+    expect(subject.bikes.size).to eq 1
   end
 
-  it 'shows docked bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
+  # it 'shows docked bike' do
+  #   bike = Bike.new
+  #   subject.dock(bike)
+  #   expect(subject.bike).to eq bike
+  # end
 
 it 'raises release bike error' do
   expect { subject.release_bike }.to raise_error "no bikes bitches"
@@ -29,7 +30,7 @@ end
 
 it 'raises dock bike error' do
   20.times { subject.dock(Bike.new)}
-  expect { subject.dock(bike) }.to raise_error "no space to dock bike"
+  expect { subject.dock(Bike.new) }.to raise_error "no space to dock bike"
 
 end
 
