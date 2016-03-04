35cf5a50b2a1a5bdf99bd5785c12ab18ef8f5cfc
Gabrielle <gabivk@gmail.com>
2016-03-04 08:29:08 +0000

    fullnesss

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,19 +8,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "no bikes bitches" if @bikes.empty?
+    raise "no bikes bitches" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "no space to dock bike" if @bikes.size == 20
+    raise "no space to dock bike" if full?
     @bikes << bike
   end
 
   private
 
+  def empty?
+    @bikes.size <= 0
+  end
+
   def full?
-    @bikes.size == 20 ? true : false
+    @bikes.size == 20
   end
 
 end
